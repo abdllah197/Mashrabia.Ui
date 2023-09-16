376c4e29fde00802f76d0337132e2a14da6bcdac
Abdllah Zayed <abdllah.zayed.work@gmail.com>
2023-09-16 13:33:30 +0300

    fixed arabic date in orders

--- src/app/shared/pipes/arabic-date.pipe.ts
+++ src/app/shared/pipes/arabic-date.pipe.ts
@@ -8,8 +8,7 @@ export class ArabicDatePipe implements PipeTransform {
 
   transform(value: Date): string
   {
-    //return value.toLocaleTimeString('ar-EG',{ hour: "2-digit", minute: "2-digit"});
-    return  new Intl.DateTimeFormat('ar-EG',{dateStyle: 'short', timeStyle: 'short'}).format(value);
+    return  new Intl.DateTimeFormat('ar-EG',{dateStyle: 'short', timeStyle: 'short'}).format(new Date(value));
   }
 
 }
